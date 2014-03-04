3ae75488a1c2433af41c376d7b40a695e054118d
mexicanrockstar <gabomorales@me.com>
2014-03-03 22:26:28 -0600

    Initial Commit

---
+++ GaTracker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GaTracker
+//
+//  Created by Rockstar. on 3/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GaTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GaTracker
+//
+//  Created by Rockstar. on 3/3/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
